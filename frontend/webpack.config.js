87fd1f3cad73e0fd3526ed34788867125662c01e
EvertonMenezesxD <everton.antonio@outlook.com>
2018-01-16 15:20:27 -0200

    ajuste webpack.config

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
         contentbase: './public'
     },
     resolve: {
-        extensions: ['', '.js', 'jsx'],
+        extensions: ['', '.js', '.jsx'],
         alias: {
             modules: __dirname + '/node_modules',
             jquery: 'modules/admin-lte/plugins/jQuery/jquery-2.2.3.min.js',
@@ -27,19 +27,21 @@ module.exports = {
         }),
         new ExtractTextPlugin('app.css')
     ],
-    module: [{
-        test: /.js[x]?$/,
-        loader: 'babel-loader',
-        exclude: 'node_modules',
-        query: {
-            presets: ['es2015', 'react'],
-            plugins: ['transform-object-spread']
-        }
-    }, {
-        test: /\.css$/,
-        loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
-    }, {
-        test: /\.woff|.woff2|.ttf|.eot|.svg|.png|.jpg*.*$/,
-        loader: 'file'
-    }]
+    module: {
+        loaders: [{
+            test: /.js[x]?$/,
+            loader: 'babel-loader',
+            exclude: 'node_modules',
+            query: {
+                presets: ['es2015', 'react'],
+                plugins: ['transform-object-rest-spread']
+            }
+        }, {
+            test: /\.css$/,
+            loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
+        }, {
+            test: /\.woff|.woff2|.ttf|.eot|.svg|.png|.jpg*.*$/,
+            loader: 'file'
+        }]
+    }
 }
