e09a4fde5b298a4cb17156248292c7dabf484165
EvertonMenezesxD <everton.antonio@outlook.com>
2018-01-26 11:25:13 -0200

    Frontend Dev Value box

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default props => (
+    <footer className="main-footer">
+        <strong>
+            Copyright &copy; 2017
+            <a href="http://localhost:8080" target='_blank'><b> M</b>MoneyApp</a>
+        </strong>
+    </footer>
+)
--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -1,8 +1,19 @@
 
+import '../common/template/dependencies'
 import React from 'react'
+
+import Header from '../common/template/header'
+import Sidebar from '../common/template/sidebar'
+import Footer from '../common/template/footer'
+import Routes from '../main/routes'
 
 export default props => (
     <div className='wrapper'>
-       <h1>Teste</h1>
+        <Header/>
+        <Sidebar/>
+        <div className="content-wrapper">
+            <Routes/>
+        </div>
+        <Footer/>
     </div>
 )
